fb2e512365267f182440c50e6ae4f8d2502e0439
Ionut Calofir <calofir.ionut@yahoo.com>
2018-04-28 21:20:18 +0300

    Update dataset and utils classes

--- dataset.py
+++ dataset.py
@@ -1,30 +1,148 @@
+import random
+
 class Dataset:
-  def __init__(self):
+  def __init__(self, anno_path='dataset/CelebA/anno',
+                     imgs_path='dataset/CelebA/imgs'):
     self._x_train = None
     self._y_train = None
     self._x_val = None
     self._y_val = None
     self._x_test = None
     self._y_test = None
+    self.labels = {'Black Hair': 0,
+                   'Blond Hair': 1,
+                   'Male': 2,
+                   'Female': 3,
+                   'Young': 4,
+                   'Old': 5,
+                   'Smile': 6}
+    self.anno_path = anno_path
+    self.imgs_path = imgs_path
 
   def get_train_batch(self, n_batch=None):
     if self._x_train is None and self._y_train is None:
       self._get_dataset()
-    pass
+
+    if n_batch is None:
+      return self._x_train, self._y_train
+
+    n_random = [random.randint(0, len(self._x_train) - 1)
+                for i in list(range(0, n_batch))]
+    x_batch = [self.imgs_path + '/' + self._x_train[i] for i in n_random]
+    y_batch = [self._y_train[i] for i in n_random]
+    return x_batch, y_batch
 
   def get_val_batch(self, n_batch=None):
     if self._x_val is None and self._y_val is None:
       self._get_dataset()
-    pass
+
+    if n_batch is None:
+      return self._x_val, self._y_val
+
+    n_random = [random.randint(0, len(self._x_val) - 1)
+                for i in list(range(0, n_batch))]
+    x_batch = [self.imgs_path + '/' + self._x_val[i] for i in n_random]
+    y_batch = [self._y_val[i] for i in n_random]
+    return x_batch, y_batch
 
   def get_test_batch(self, n_batch=None):
     if self._x_test is None and self._y_test is None:
       self._get_dataset()
-    pass
+
+    if n_batch is None:
+      return self._x_test, self._y_test
+
+    n_random = [random.randint(0, len(self._x_test) - 1)
+                for i in list(range(0, n_batch))]
+    x_batch = [self.imgs_path + '/' + self._x_test[i] for i in n_random]
+    y_batch = [self._y_test[i] for i in n_random]
+    return x_batch, y_batch
 
   def _get_dataset(self):
-    pass
+    self._x_test = []
+    self._y_test = []
+    with open(self.anno_path + '/test.txt') as f:
+      for line in f:
+        img_line = line.rstrip().split()
+        self._x_test.append(img_line[0])
+        self._y_test.append([int(i) for i in img_line[1:]])
+
+    self._x_val = []
+    self._y_val = []
+    with open(self.anno_path + '/val.txt') as f:
+      for line in f:
+        img_line = line.rstrip().split()
+        self._x_val.append(img_line[0])
+        self._y_val.append([int(i) for i in img_line[1:]])
+
+    self._x_train = []
+    self._y_train = []
+    with open(self.anno_path + '/train.txt') as f:
+      for line in f:
+        img_line = line.rstrip().split()
+        self._x_train.append(img_line[0])
+        self._y_train.append([int(i) for i in img_line[1:]])
 
   @staticmethod
-  def build_dataset():
-    pass
+  def build_dataset_celeba(anno_path='dataset/CelebA/anno',
+                           imgs_path='dataset/CelebA/imgs',
+                           train_percent=80,
+                           val_percent=19,
+                           test_percent=1):
+    target_labels = {'Black_Hair': 'Black Hair',
+                    'Blond_Hair': 'Blond Hair',
+                    'Male': 'Male',
+                    'Young': 'Young',
+                    'Smiling': 'Smile'}
+    imgs_name = []
+    imgs_labels = []
+    with open(anno_path + '/original.txt') as f:
+      n_imgs = int(f.readline().rstrip())
+      anno_labels = f.readline().rstrip().split()
+      anno_labels_dict = {k: v + 1 for v, k in enumerate(anno_labels)}
+      for line in f:
+        img_line = line.rstrip().split()
+
+        imgs_name.append(img_line[0]) # img[0] - name of the image
+
+        img_labels = [0 for i in list(range(len(Dataset().labels.items())))]
+        for k, v in target_labels.items():
+          anno_labels_pos = anno_labels_dict[k]
+          if int(img_line[anno_labels_pos]) == 1:
+            img_labels[Dataset().labels[v]] = 1
+          else:
+            if k == 'Male':
+              img_labels[Dataset().labels['Female']] = 1
+            elif k == 'Young':
+              img_labels[Dataset().labels['Old']] = 1
+        imgs_labels.append(img_labels)
+      tup = list(zip(imgs_name, imgs_labels))
+      random.shuffle(tup)
+      imgs_name, imgs_labels = zip(*tup)
+
+    n_test = int(n_imgs * test_percent / 100) + 1
+    n_val = int(n_imgs * val_percent / 100) + 1
+
+    x_test = imgs_name[:n_test]
+    y_test = imgs_labels[:n_test]
+
+    x_val = imgs_name[n_test:n_val]
+    y_val = imgs_labels[n_test:n_val]
+
+    x_train = imgs_name[n_val:]
+    y_train = imgs_labels[n_val:]
+
+    with open(anno_path + '/test.txt', 'w') as f:
+      for i in list(range(len(x_test))):
+        img_line = x_test[i] + ' ' + ' '.join([str(l) for l in y_test[i]])
+        f.write(img_line + '\n')
+
+    with open(anno_path + '/val.txt', 'w') as f:
+      for i in list(range(len(x_val))):
+        img_line = x_val[i] + ' ' + ' '.join([str(l) for l in y_val[i]])
+        f.write(img_line + '\n')
+
+    with open(anno_path + '/train.txt', 'w') as f:
+      for i in list(range(len(x_train))):
+        img_line = x_train[i] + ' ' + ' '.join([str(l) for l in y_train[i]])
+        f.write(img_line + '\n')
--- utils.py
+++ utils.py
@@ -1,5 +1,128 @@
+import cv2
+import random
+import numpy as np
+
 from dataset import Dataset
 
 class Utils:
   def __init__(self):
-    ds = Dataset()
+    self.ds = Dataset()
+    self._y_target = None
+
+  def next_batch_train(self, n_batch=None):
+    x_batch = []
+    y_batch = []
+
+    x_train, y_train = self.ds.get_train_batch(n_batch)
+    y_target = self.y_target(len(y_train[0]), len(y_train))
+    for x in x_train:
+      x_img = cv2.imread(x)
+      x_img = cv2.cvtColor(x_img, cv2.COLOR_BGR2RGB)
+      x_img = self._crop_and_resize_img_celeba(x_img, 178)
+      x_img = self.normalize_img(x_img)
+
+      x_batch.append(x_img)
+
+    y_batch = np.reshape(y_train, (-1, 1, 1, len(y_train[0])))
+    y_target_batch = np.reshape(y_target, (-1, 1, 1, len(y_target[0])))
+
+    x_batch = np.array(x_batch)
+
+    return x_batch.astype(np.float64), \
+           y_batch.astype(np.float64), \
+           y_target_batch.astype(np.float64)
+
+  def next_batch_val(self, n_batch=None):
+    x_batch = []
+    y_batch = []
+
+    x_val, y_val = self.ds.get_val_batch(n_batch)
+    y_target = self.y_target(len(y_val[0]), len(y_val))
+    for x in x_val:
+      x_img = cv2.imread(x)
+      x_img = cv2.cvtColor(x_img, cv2.COLOR_BGR2RGB)
+      x_img = self._crop_and_resize_img_celeba(x_img, 178)
+      x_img = self.normalize_img(x_img)
+
+      x_batch.append(x_img)
+
+    y_batch = np.reshape(y_val, (-1, 1, 1, len(y_val[0])))
+    y_target_batch = np.reshape(y_target, (-1, 1, 1, len(y_target[0])))
+
+    x_batch = np.array(x_batch)
+
+    return x_batch.astype(np.float64), \
+           y_batch.astype(np.float64), \
+           y_target_batch.astype(np.float64)
+
+  def next_batch_test(self, n_batch=None):
+    x_batch = []
+    y_batch = []
+
+    x_test, y_test = self.ds.get_test_batch(n_batch)
+    y_target = self.y_target(len(y_test[0]), len(y_test))
+    for x in x_test:
+      x_img = cv2.imread(x)
+      x_img = cv2.cvtColor(x_img, cv2.COLOR_BGR2RGB)
+      x_img = self._crop_and_resize_img_celeba(x_img, 178)
+      x_img = self.normalize_img(x_img)
+
+      x_batch.append(x_img)
+
+    y_batch = np.reshape(y_test, (-1, 1, 1, len(y_test[0])))
+    y_target_batch = np.reshape(y_target, (-1, 1, 1, len(y_target[0])))
+
+    x_batch = np.array(x_batch)
+
+    return x_batch.astype(np.float64), \
+           y_batch.astype(np.float64), \
+           y_target_batch.astype(np.float64)
+
+  def y_target(self, n_labels, n_batch):
+    if self._y_target is None:
+      nrs = [i for i in list(range(0, 2 ** n_labels))]
+      random.shuffle(nrs)
+
+      self._y_target = []
+      for nr in nrs:
+        label = [int(c) for c in bin(nr)[2:]]
+
+        diff = n_labels - len(label)
+        for z in list(range(diff)):
+          label.insert(0, 0)
+
+        self._y_target.append(label)
+
+    n_random = [random.randint(0, len(self._y_target) - 1)
+                for i in list(range(0, n_batch))]
+    y_batch = [self._y_target[i] for i in n_random]
+
+    return y_batch
+
+  def _crop_and_resize_img_celeba(self, img, height):
+    height_diff = int((img.shape[0] - height) / 2)
+    img = img[height_diff:-height_diff, :, :]
+    img = cv2.resize(img, (128, 128))
+
+    return img
+
+  def normalize_img(self, img):
+    img = img.astype(np.float64)
+    img = img / 127.5 - 1
+
+    return img
+
+  def denormalize_img(self, img):
+    img = (img + 1) * 127.5
+    img[img > 255.] = 255.
+    img[img < 0.] = 0.
+    img = img.astype(np.uint8)
+
+    return img
+
+  def show_img(self, img):
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
