8dd573912c201557aa26c993cda7c45d49e6eabb
Ionut Calofir <calofir.ionut@yahoo.com>
2018-05-01 03:29:35 +0300

    Add arguments for main

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import argparse
+
 from starGAN import StarGAN
 
-model = StarGAN(batch_size=8)
-model.build()
-model.train()
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument('--train', help='if true, start training a neural network',
+                                 action='store_true')
+  parser.add_argument('--resume', help='if true, resume training from last checkpoint',
+                                  action='store_true')
+  parser.add_argument('--batchSize', help='batch size for training, default=8',
+                                     type=int,
+                                     default=8)
+  args = parser.parse_args()
+
+  if args.train:
+    model = StarGAN(batch_size=args.batchSize)
+    model.build()
+
+    if args.resume:
+      model.train(resume=True)
+    else:
+      model.train()
+  else:
+    print('Nothing to be done!')
